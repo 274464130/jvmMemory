9a826614e0d792e7963a2ae0abd659f50bb8f2b9
wppc <274464130@qq.com>
2020-05-13 23:10:57 +0800

    new  object  内存分布

--- src/main/java/com/example/demo/storage/MemoryLayout.java
+++ src/main/java/com/example/demo/storage/MemoryLayout.java
@@ -1,4 +1,14 @@
 package com.example.demo.storage;
 
+import org.openjdk.jol.info.ClassLayout;
+
 public class MemoryLayout {
+    public static void main(String[] args) {
+        Object o = new Object();
+        // new object的  内存分布
+
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+    }
+
+
 }
