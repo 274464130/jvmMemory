f3f702dada3f755b5b4d69768191143203d33cb9
wppc <274464130@qq.com>
2020-05-13 23:14:27 +0800

    new  obejct  添加注释

--- src/main/java/com/example/demo/storage/MemoryLayout.java
+++ src/main/java/com/example/demo/storage/MemoryLayout.java
@@ -6,6 +6,8 @@ public class MemoryLayout {
     public static void main(String[] args) {
         Object o = new Object();
         // new object的  内存分布
+        // 1 markword   8个字节    2 类的指针   class  pointer  3   实例数据    4  padding  可有可无
+        //加起来一定可以被8整除
 
         System.out.println(ClassLayout.parseInstance(o).toPrintable());
     }
